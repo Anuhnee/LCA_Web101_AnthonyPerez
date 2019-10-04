183ab08072f3970d40fc63b71291e3bbf1b7ffb3
Anthony Perez <antrp93@gmail.com>
2019-10-04 00:38:16 -0500

    geolocation
    Added more functionality to the TodoApp and some styling to the TodoApp form

--- myPortfolio/Scripts/TodoApp.js
+++ myPortfolio/Scripts/TodoApp.js
@@ -1,7 +1,43 @@
-$(function() {
-  var newEntry = "<li>  </li>";
+// $(function() {
+//   var newEntry = "<li>  </li>";
 
-  $("#addBtn").click(function() {
-    $("#myUL").append(newEntry);
+//   $("#addBtn").click(function() {
+//     $("#myUL").append(newEntry);
+//   });
+// });
+
+$(function() {
+  let $list = $(".TodoUl");
+  let $newItemForm = $("#newItemForm");
+
+  $newItemForm.on("submit", function(e) {
+    e.preventDefault();
+    let text = $('input.myTodo[type="text"]').val();
+    $list.append(`<li>${text}</li>`);
+    $('input[type="text"]').val("");
+    $(`li`).addClass("ulItem");
   });
+
+  $list.on("click", "li", function() {
+    let $this = $(this);
+    $this.remove();
+  });
+
+  var myLocation = document.getElementById("my-coordinates");
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(displayPosition);
+    } else {
+      myLocation.innerHTML = "Not available";
+    }
+  }
+
+  function displayPosition(position) {
+    myLocation.innerHTML =
+      "Longitude: " +
+      position.coords.longitude +
+      " Latitude: " +
+      position.coords.latitude;
+  }
 });
